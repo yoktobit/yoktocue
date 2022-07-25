780bc3f43847327f1cecec520b0bcfe15ad36c6b
yoktobit <martin@yoktobit.de>
2022-07-25 21:44:19 +0200

    add parenthesises

--- templates/router/router.go
+++ templates/router/router.go
@@ -5,6 +5,6 @@ import "github.com/labstack/echo/v4"
 func SetupRouter(e *echo.Echo) {
 
 	{{ range $R := .Routes }}
-	e.{{upper $R.Method}}({{$R.Path}}, handle{{$R.Name}})
+	e.{{upper $R.Method}}("{{$R.Path}}", handle{{$R.Name}})
 	{{ end }}
 }
